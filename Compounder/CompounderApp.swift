c6b55e69dc99610af903a769e74296029a292e57
wiseguy16 <gergusa04@gmail.com>
2022-12-07 13:43:10 -0500

    Initial Commit

---
+++ Compounder/CompounderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompounderApp.swift
+//  Compounder
+//
+//  Created by Gregory Weiss on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CompounderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
